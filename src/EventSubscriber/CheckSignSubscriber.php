1c86ec74341833b347df24aa055f6a40dc27f50a
tourze-ci <you@example.com>
2025-04-15 09:17:45 +0000

    feat(json-rpc): 添加签名检查功能和Signer服务

---
+++ src/EventSubscriber/CheckSignSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tourze\JsonRPCSignBundle\EventSubscriber;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Tourze\DoctrineHelper\ReflectionHelper;
+use Tourze\JsonRPC\Core\Event\BeforeMethodApplyEvent;
+use Tourze\JsonRPCSignBundle\Attribute\CheckSign;
+use Tourze\JsonRPCSignBundle\Service\Signer;
+
+/**
+ * 签名检查，参考了阿里云开放服务的签名设计，做了一些调整。
+ * 如果开启签名的话，接口变动可能很多，所以暂时没全部使用到。
+ *
+ * 签名算法：
+ * 1. 将AppId加入到header，示例 Signature-AppID: 111111
+ * 2. 获取一个32位内的随机字符串，传入header。如 Signature-Nonce: test123
+ * 3. 获取当前系统时间戳，传入header。如 Signature-Timestamp: 1678856989
+ * 4. 将要发送出去的payload、时间戳、Nonce合并成一个字符串，然后使用HMAC-SHA1生成签名，key使用AppSecret，参考代码 hash_hmac('sha1', $rawText, (string) $caller->getAppSecret());；
+ * 5. 签名字符串加入到header，如 Signature: f383cba92442ad95f8ce7d61f6d5da5661381b6d
+ *
+ * @see https://help.aliyun.com/document_detail/131955.html
+ */
+class CheckSignSubscriber
+{
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly Signer $signer,
+        private readonly LoggerInterface $procedureLogger,
+    ) {
+    }
+
+    #[AsEventListener]
+    public function beforeMethodApply(BeforeMethodApplyEvent $event): void
+    {
+        $CheckSign = ReflectionHelper::getClassReflection($event->getMethod())->getAttributes(CheckSign::class);
+        if (!$CheckSign) {
+            // 不需要做签名处理
+            return;
+        }
+
+        // TODO 这个对象最好是通过事件来传递
+        $request = $this->requestStack->getMainRequest();
+        if ($request->query->get('__ignoreSign') === ($_ENV['JSON_RPC_GOD_SIGN'] ?? 'god')) {
+            return;
+        }
+
+        $this->signer->checkRequest($request);
+
+        $this->procedureLogger->info('签名校验通过，允许访问接口', [
+            'method' => $event->getMethod(),
+            'request' => $request,
+        ]);
+    }
+}
---
+++ src/Service/Signer.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Tourze\JsonRPCSignBundle\Service;
+
+use Carbon\Carbon;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Tourze\JsonRPCCallerBundle\Repository\ApiCallerRepository;
+use Tourze\JsonRPCSignBundle\Exception\SignAppIdMissingException;
+use Tourze\JsonRPCSignBundle\Exception\SignAppIdNotFoundException;
+use Tourze\JsonRPCSignBundle\Exception\SignErrorException;
+use Tourze\JsonRPCSignBundle\Exception\SignNonceMissingException;
+use Tourze\JsonRPCSignBundle\Exception\SignRequiredException;
+use Tourze\JsonRPCSignBundle\Exception\SignTimeoutException;
+
+/**
+ * 签名处理工具
+ *
+ * 一般来说，我们都是对整个Request来进行签名计算
+ */
+class Signer
+{
+    public function __construct(
+        private readonly ApiCallerRepository $callerRepository,
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    public function getRequestNonce(Request $request): string
+    {
+        $str = $request->headers->get('Signature-Nonce');
+        if (empty($str)) {
+            throw new SignNonceMissingException();
+        }
+
+        return $str;
+    }
+
+    public function getRequestSignatureMethod(Request $request): string
+    {
+        return $request->headers->get('Signature-Method', 'HMAC-SHA1');
+    }
+
+    public function getRequestSignatureVersion(Request $request): string
+    {
+        return $request->headers->get('Signature-Version', '1.0');
+    }
+
+    public function getRequestSignatureAppId(Request $request): string
+    {
+        $SignatureAppID = $request->headers->get('Signature-AppID');
+        if (empty($SignatureAppID)) {
+            throw new SignAppIdMissingException();
+        }
+
+        return $SignatureAppID;
+    }
+
+    /**
+     * 检查请求是否签名通过
+     *
+     * @throws SignAppIdMissingException
+     * @throws SignAppIdNotFoundException
+     * @throws SignErrorException
+     * @throws SignNonceMissingException
+     * @throws SignRequiredException
+     * @throws SignTimeoutException
+     */
+    public function checkRequest(Request $request): void
+    {
+        $SignatureAppID = $this->getRequestSignatureAppId($request);
+        $caller = $this->callerRepository->findOneBy([
+            'appId' => $SignatureAppID,
+            'valid' => true,
+        ]);
+        if (empty($caller)) {
+            throw new SignAppIdNotFoundException();
+        }
+
+        $nonce = $this->getRequestNonce($request);
+        $signMethod = $this->getRequestSignatureMethod($request);
+        $signVersion = $this->getRequestSignatureVersion($request);
+        $signType = "{$signMethod}-{$signVersion}";
+
+        // 如果客户端时间跟服务端时间相差太大，就不允许继续
+        $SignatureTimestamp = $request->headers->get('Signature-Timestamp', '');
+        $tolerateSeconds = $caller->getSignTimeoutSecond() ?: 60 * 3; // 默认允许3分钟误差
+        if (abs(Carbon::now()->getTimestamp() - $SignatureTimestamp) > $tolerateSeconds) {
+            throw new SignTimeoutException();
+        }
+
+        $Signature = $request->headers->get('Signature');
+        if ($Signature) {
+            $ServerSign = null;
+            $rawText = '';
+            if ('md5-1.0' === $signType) {
+                // 这里的签名很简单，只是将payload+时间戳+随机字符串+秘钥合并起来
+                $rawText = implode('', [
+                    $request->getContent(),
+                    $SignatureTimestamp,
+                    $nonce,
+                    $caller->getAppSecret(),
+                ]);
+                $ServerSign = md5($rawText);
+            }
+
+            if ('HMAC-SHA1-1.0' === $signType || 'sha1-1.0' === $signType) {
+                $rawText = implode('', [
+                    $request->getContent(),
+                    $SignatureTimestamp,
+                    $nonce,
+                ]);
+                $ServerSign = hash_hmac('sha1', $rawText, (string) $caller->getAppSecret());
+            }
+
+            if (null === $ServerSign) {
+                // 找不到对应算法，说明可能不支持这个签名算法
+                throw new SignErrorException();
+            }
+
+            if ($ServerSign !== $Signature) {
+                $this->logger->warning('JsonRPC签名不通过', [
+                    'serverSign' => $ServerSign,
+                    'submitSign' => $Signature,
+                    'rawText' => $rawText,
+                ]);
+                throw new SignErrorException();
+            }
+        } else {
+            throw new SignRequiredException();
+        }
+    }
+}
